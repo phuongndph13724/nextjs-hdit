6e1168b18369f7ae88ba7720dcb00e23c3277ea4
phuongnd <dacphuongotp@gmail.com>
2024-12-05 15:01:20 +0700

    Update send email verify code when login

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -5,22 +5,28 @@ import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { authenticate } from "@/utils/actions";
 import { useRouter } from "next/navigation";
+import ModalReactive from "./modal.reactive";
+import { useState } from "react";
 
 const Login = () => {
   const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
   const onFinish = async (formData: any) => {
     const { email, password } = formData;
-
+    setUserEmail("");
     const response = await authenticate(email, password);
     if (response.error) {
-      notification.error({
-        message: "Login error!",
-        description: response.error,
-      });
       if (response.code === 401) {
+        setUserEmail(email);
         // statusCode;
-        router.push("/verify/2321321");
+        setOpen(true);
+      } else {
+        notification.error({
+          message: "Login error!",
+          description: response.error,
+        });
       }
     } else {
       router.push("/dashboard");
@@ -28,66 +34,69 @@ const Login = () => {
   };
 
   return (
-    <Row justify={"center"} style={{ marginTop: "30px" }}>
-      <Col xs={24} md={16} lg={8}>
-        <fieldset
-          style={{
-            padding: "15px",
-            margin: "5px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-          }}
-        >
-          <legend>Đăng Nhập</legend>
-          <Form
-            name="basic"
-            onFinish={onFinish}
-            autoComplete="off"
-            layout="vertical"
+    <>
+      <Row justify={"center"} style={{ marginTop: "30px" }}>
+        <Col xs={24} md={16} lg={8}>
+          <fieldset
+            style={{
+              padding: "15px",
+              margin: "5px",
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+            }}
           >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email!",
-                },
-              ]}
+            <legend>Đăng Nhập</legend>
+            <Form
+              name="basic"
+              onFinish={onFinish}
+              autoComplete="off"
+              layout="vertical"
             >
-              <Input autoComplete="true" />
-            </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your email!",
+                  },
+                ]}
+              >
+                <Input autoComplete="true" />
+              </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                },
-              ]}
-            >
-              <Input.Password autoComplete="true" />
-            </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password autoComplete="true" />
+              </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-          <Link href={"/"}>
-            <ArrowLeftOutlined /> Quay lại trang chủ
-          </Link>
-          <Divider />
-          <div style={{ textAlign: "center" }}>
-            Chưa có tài khoản?{" "}
-            <Link href={"/auth/register"}>Đăng ký tại đây</Link>
-          </div>
-        </fieldset>
-      </Col>
-    </Row>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+            <Link href={"/"}>
+              <ArrowLeftOutlined /> Quay lại trang chủ
+            </Link>
+            <Divider />
+            <div style={{ textAlign: "center" }}>
+              Chưa có tài khoản?{" "}
+              <Link href={"/auth/register"}>Đăng ký tại đây</Link>
+            </div>
+          </fieldset>
+        </Col>
+      </Row>
+      <ModalReactive open={open} setOpen={setOpen} userEmail={userEmail} />
+    </>
   );
 };
 
